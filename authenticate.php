d6fc1783cde94aa246299cf7dbd11c78dea343c7
lyssadeveloper <lyssamailbox@gmail.com>
2023-05-04 22:02:11 -0400

    feat: authenticate user revtrieving dynamically from databse

--- authenticate.php
+++ authenticate.php
@@ -1,24 +1,57 @@
 <?php
-    session_set_cookie_params(['httponly' => true]);
+// 0-Criar a sessão
+session_set_cookie_params(['httponly' => true]);
 
-    session_start();
+session_start();
 
-    session_regenerate_id();
+session_regenerate_id();
 
-    $user_default     = "lyssautida";
-    $password_default = "12345678";
+// 1-Importar conexão bd
+include 'conexao.php';
 
-    $user_typed     = $_POST["email"];
-    $password_typed = $_POST["password"];
+// 2-Receber variável do input de login
+   $input_email     = $_POST["email"];
+   $input_password = $_POST["password"];
 
-     if($user_default == $user_typed) {
-        if($password_default == $password_typed) {
-            $_SESSION["logged_user"] = $user_typed;
+// 3-Query de busca de dados
+    $buscar_usuario = "SELECT * FROM users WHERE email = '$input_email'";
+
+// 4-Statment prepara a query
+    $stmt = $conexao -> prepare($buscar_usuario);
+    
+// 5-Executar statment
+    $stmt->execute();
+
+// 6-Receber os dados do statment e armazenar em variáveis
+    $receber_cadastros = $stmt -> fetch(PDO::FETCH_ASSOC);
+    #var_dump($receber_cadastros);
+
+    $id        = $receber_cadastros['id'];
+    $email     = $receber_cadastros['email'];
+    $senha     = $receber_cadastros['senha'];
+    $nome      = $receber_cadastros['nome'];
+    $telefone  = $receber_cadastros['telefone'];
+    $cidade    = $receber_cadastros['cidade'];
+    $estado    = $receber_cadastros['estado'];
+
+// 7-Armazenar em um array sem a senha
+    $found_user = [
+        'id'       => $id,
+        'email'    => $email,
+        'nome'     => $nome,
+        'telefone' => $telefone,
+        'cidade'   => $cidade,
+        'estado'   => $estado,
+    ];
+
+    if($email == $input_email) {
+        if($senha == $input_password) {
+            $_SESSION["logged_user"] = $found_user;
             header("location: index.php");
         } else {
             header("location: login.html?failed=true");
         }
-     } else {
+    } else {
         header("location: login.html?failed=true");
     }
 ?>
